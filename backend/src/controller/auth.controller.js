6adc3c26344ea462bee6b28e5d3b925830f28795
BALAMURALI-404 <bbalamurali2004@gmail.com>
2025-07-26 00:23:31 +0530

    initial commit

---
+++ backend/src/controller/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req,res) => {
+    res.send("Signup request");
+}
+
+export const login = (req,res) => {
+    res.send("login request");
+}
+
+export const logout = (req,res) => {
+    res.send("logout request");
+}
