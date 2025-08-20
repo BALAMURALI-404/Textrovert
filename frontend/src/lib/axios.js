3544bf096064b088f45dfe4a9d06e6c9190faa72
BALAMURALI-404 <bbalamurali2004@gmail.com>
2025-08-20 22:17:49 +0530

    vrvsw

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api', // Base URL for your API
+    baseURL: 'https://textrovert-pghq.onrender.com/api', // Base URL for your API
     withCredentials: true, // Include credentials in requests
 })
